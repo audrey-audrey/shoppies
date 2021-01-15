888d316db7589095001b2737859f7464756b6bf7
Audrey <audreyuu@gmail.com>
2021-01-15 16:20:30 -0500

    Built search form components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 import MovieListItem from './components/MovieListItem';
+import SearchForm from './components/SearchForm'
 
 function App() {
   const [currentMovie, setCurrentMovie] = useState("")
+
+  const updateMovie = movie => setCurrentMovie(movie)
+
   return (
     <div className="App">
+      <SearchForm {...{updateMovie}} />
       <MovieListItem />
     </div>
   );
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
 
 export default function SearchForm(props) {
+  const {updateMovie} = props
   const [movie, setMovie] = useState("")
 
   const handleChange = (event) => {
     const valueTyped = event.target.value
 
     if (valueTyped) {
-      setCity(valueTyped)
+      setMovie(valueTyped)
     }
   }
 
-  
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    updateMovie(movie)
+  }
 
   return (
-    <form>
-      <input type="text" name="movie" placeholder="Enter movie name" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="movie" placeholder="Enter movie name" onChange={handleChange}/>
       <button type="submit">Find the movie!</button>
     </form>
   )
